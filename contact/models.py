3db5581133a3aabd1c4ae1ab29979481d8502d47
sarah13310 <xenatronics@laposte.net>
2024-02-05 22:08:47 +0100

    Version 1.0.1

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import (
+    FieldPanel, FieldRowPanel,
+    InlinePanel, MultiFieldPanel
+)
+from wagtail.fields import RichTextField
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey('ContactPage', on_delete=models.CASCADE, related_name='form_fields')
+
+
+class ContactPage(AbstractEmailForm):
+    
+    introduction = RichTextField(blank=True)
+    
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('introduction'),
+        InlinePanel('form_fields', label="Champs du Formulaire"),
+    ]
